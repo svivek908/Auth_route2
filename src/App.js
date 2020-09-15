3d1ecb0273e9533cff0ecd80cde9ff14513fa91f
svivek908 <svivek908@gmail.com>
2020-09-15 18:52:23 +0530

    redux-crud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
-import {Switch, Route, BrowserRouter as Router, Redirect} from 'react-router-dom';
-import Dashboard from './components/Dashboard'
-import Login from './components/Login';
+import Contacts from './components/elements/contact/Contacts'
+import Navbar from './components/elements/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-import ProtectedRoute from './components/ProtectedRoute'
-import Notefound from './components/Notefound';
+import {Provider} from 'react-redux'
+import store from './Store'
+import './styles/App.scss';
+import Contact from './components/elements/contact/Contact';
+import ContactsAdd from './components/elements/contact/ContactsAdd';
+import EditContact from './components/elements/EditContact';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Router>
-        
-        <Switch>
-        <Route exact path="/" component={Login}/>
-        
-        <ProtectedRoute exact path="/Dashboard" component={Dashboard}/>
-         </Switch>
-        </Router>
+      <Provider store={store}>
+       <Router>
+       <div>
+          <Navbar/>
+          <div className="container">
+            <div className="py-3">
+                <Switch>
+                  <Route exact path="/" component={Contacts}/>
+                  <Route exact path="/contacts/add" component={ContactsAdd}/>
+                  <Route exact path="/contacts/edit/:id" component={EditContact}/>
+                </Switch>
+            </div>
+          </div>
       </div>
+       </Router>
+      </Provider>
     )
   }
 }
--- src/components/Notefound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Notefound() {
-    return (
-        <div>
-            <h1>Not found</h1>
-        </div>
-    )
-}
